96b7e6ecaab82e891174e6592cf5ca32a85ba7b6
mramalmesbaihe <mramalmesbahi@gmail.com>
2025-11-16 22:48:37 +0300

    update

---
+++ New folder/2.py
@@ -0,0 +1,14 @@
+username = 'bassam'
+password = 1234
+
+if username == 'bassam' and password == 1234:
+    print("welcome bassam")
+
+elif username == 'bassam':
+    print("invalid password")
+
+elif password == 1234:
+    print("invalid username")
+
+else:
+    print("welcome ahmed")
---
+++ python-projects/اله حاسبه.py
@@ -0,0 +1,25 @@
+num1 = float(input( "Enter the first number: "))
+num2 = float(input("enter the second number: "))
+
+operation = input("Select the operation(+ - * /): ")
+
+if operation == "+":
+    result = num1 + num2
+elif operation == "-":
+    result = num1 - num2
+elif operation == "*":
+    result = num1 * num2
+elif operation == "/":
+    result = num1 / num2
+else:
+    print("the option is not right")
+    exit()
+
+print("result:", result)
+
+if num1 > num2:
+    print("the first number is bigger than a second number")
+elif num1 < num2:
+    print("the first number is the smaller than second number")
+else:
+    print("both numbers are equal")
